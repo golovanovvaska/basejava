fea736a8d554cfe0343a29670326acf6299e8272
vasiliy <golovanovvaska@gmail.com>
2023-11-15 18:51:06 +0300

    исправил замечания

--- src/com/basejava/webapp/DeadLock.java
+++ src/com/basejava/webapp/DeadLock.java
@@ -5,9 +5,14 @@ public class DeadLock {
     private final static Object LOCK2 = new Object();
 
     public static void main(String[] args) {
+        deadLock(LOCK1, LOCK2);
+        deadLock(LOCK2, LOCK1);
+    }
+
+    private static void deadLock(Object... lock) {
         new Thread(() -> {
             System.out.println(Thread.currentThread().getName() + " waiting for LOCK1");
-            synchronized (LOCK1) {
+            synchronized (lock[0]) {
                 System.out.println(Thread.currentThread().getName() + " holding LOCK1");
                 try {
                     Thread.sleep(10);
@@ -15,23 +20,7 @@ public class DeadLock {
                     throw new RuntimeException(e);
                 }
                 System.out.println(Thread.currentThread().getName() + " waiting for LOCK2");
-                synchronized (LOCK2) {
-                    System.out.println(Thread.currentThread().getName() + " holding LOCK1 & LOCK2");
-                }
-            }
-        }).start();
-
-        new Thread(() -> {
-            System.out.println(Thread.currentThread().getName() + " waiting for LOCK2");
-            synchronized (LOCK2) {
-                System.out.println(Thread.currentThread().getName() + " holding LOCK2");
-                try {
-                    Thread.sleep(10);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-                System.out.println(Thread.currentThread().getName() + " waiting for LOCK1");
-                synchronized (LOCK1) {
+                synchronized (lock[1]) {
                     System.out.println(Thread.currentThread().getName() + " holding LOCK1 & LOCK2");
                 }
             }
